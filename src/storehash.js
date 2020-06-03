a1bf0a0ee5ac3bc4837569f5df4c868a5c7c0a12
jorge Ivan Reyes <jorgeivanreyesvalencia92@gmail.om>
2020-06-03 12:58:10 -0500

    ipfsprueba

---
+++ src/storehash.js
@@ -0,0 +1,37 @@
+import web3 from './web3';
+
+//access our local copy to contract deployed on rinkeby testnet
+//use your own contract address
+const address = '0x5643Ef1d9F56c25dB9A8f3326890FF7A44aEb1b7';
+//use the ABI from your contract
+const abi = [
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "getHash",
+    "outputs": [
+      {
+        "name": "x",
+        "type": "string"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "x",
+        "type": "string"
+      }
+    ],
+    "name": "sendHash",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  }
+]
+export default new web3.eth.Contract(abi, address);
